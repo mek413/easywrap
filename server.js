3a3cceea44c5491216341e877e9d3dfcc9f35db4
Marcus Kelly <mek413@gmail.com>
2020-01-18 00:05:21 -0600

    deployment

--- server.js
+++ server.js
@@ -50,9 +50,9 @@ app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
 
 // Listener
 http.listen(PORT, function () {
