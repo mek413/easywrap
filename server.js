56e5737ec92d636a5b1f14c4742102d2464e988b
Marcus Kelly <mek413@gmail.com>
2020-01-18 00:26:37 -0600

    deployment

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ passport.deserializeUser(db.User.deserializeUser());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 }
 
 // Connect to MongoDB
@@ -50,9 +50,9 @@ app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 // Listener
 http.listen(PORT, function () {
